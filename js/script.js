b2098013c0c7a05a1a5f9d973f2e642f12c0692f
Wuqian Ye <natsumi3613@qq.com>
2017-12-04 23:21:07 +0000

    js

--- js/script.js
+++ js/script.js
@@ -27,6 +27,28 @@
 
 
 // DOCUMENT READY FUNCTION
-$(document).ready({
+$(document).ready(function(){
+        var computerChoice = "";
+        var winner = "";
+        var choices = ["rock","paper","scissor"];
         
+        $("input").keyup(function(event) {
+                if (event.keyCode === 13) {
+                        $("button").click();
+                }
+        });
+        
+        $("button").click(function(){
+                var userChoice = $("input").val();
+                if(userChoice === choices[0]){
+                        $("#pchoice").html('<img src="https://previews.123rf.com/images/julinzy/julinzy1310/julinzy131000173/23262897-hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white--Stock-Photo.jpg"/>');
+                }else if(userChoice === choices[1]){
+                        $("#pchoice").html('<img src="https://previews.123rf.com/images/julinzy/julinzy1310/julinzy131000180/23262904-hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white-background-Stock-Vector.jpg"/>');
+                }else if(userChoice === choices[2]){
+                        $("#pchoice").html('<img src="https://previews.123rf.com/images/julinzy/julinzy1310/julinzy131000181/23262905-hand-sign-of-rock-paper-scissors-game-isolated-vector-on-white-background-Stock-Vector.jpg"/>');
+                }else{
+                        $("#pchoice").html("Please enter rock, paper, or scissor.")
+                }
+                $("input").val("");
+        });
 });
